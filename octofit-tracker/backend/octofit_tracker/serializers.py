d24a27b8d633c94235e06725ef2943c987180edb
Hugo Reis <79321113+hreis00@users.noreply.github.com>
2026-01-22 10:56:13 +0000

    ✨ feat(models): add initial models and migrations for octofit

---
+++ octofit-tracker/backend/octofit_tracker/serializers.py
@@ -0,0 +1,58 @@
+from rest_framework import serializers
+from .models import User, Team, Activity, Leaderboard, Workout
+
+
+class UserSerializer(serializers.ModelSerializer):
+    id = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = User
+        fields = ['id', 'name', 'email', 'password', 'team_id', 'created_at']
+        extra_kwargs = {'password': {'write_only': True}}
+    
+    def get_id(self, obj):
+        return str(obj.pk)
+
+
+class TeamSerializer(serializers.ModelSerializer):
+    id = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = Team
+        fields = ['id', 'name', 'description', 'created_at']
+    
+    def get_id(self, obj):
+        return str(obj.pk)
+
+
+class ActivitySerializer(serializers.ModelSerializer):
+    id = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = Activity
+        fields = ['id', 'user_id', 'activity_type', 'duration', 'distance', 'calories', 'date', 'created_at']
+    
+    def get_id(self, obj):
+        return str(obj.pk)
+
+
+class LeaderboardSerializer(serializers.ModelSerializer):
+    id = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = Leaderboard
+        fields = ['id', 'user_id', 'team_id', 'total_calories', 'total_activities', 'total_duration', 'rank', 'updated_at']
+    
+    def get_id(self, obj):
+        return str(obj.pk)
+
+
+class WorkoutSerializer(serializers.ModelSerializer):
+    id = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = Workout
+        fields = ['id', 'name', 'description', 'activity_type', 'difficulty', 'duration', 'calories_per_session', 'created_at']
+    
+    def get_id(self, obj):
+        return str(obj.pk)
